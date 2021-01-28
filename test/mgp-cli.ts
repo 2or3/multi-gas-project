dcbfc90c16f4a3dc96f4d64f937caa625e4aa746
2or3 <t.nagashima.cog@gmail.com>
2021-01-28 19:41:20 +0900

    fix template

---
+++ test/mgp-cli.ts
@@ -0,0 +1,7 @@
+import { describe, it } from "mocha";
+import { assert } from "chai";
+
+describe('コマンドを実行する', () => {
+    it('project ディレクトリが作成される', () => { });
+    it('project ディレクトリに.gitディレクトリがない', () => { });
+});
